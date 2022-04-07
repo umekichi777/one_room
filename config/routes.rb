2cf4a745d57b7c64f5314bb056f7ef74b48b3584
umekichi777 <umekichi7teru@gmail.com>
2022-04-07 08:21:47 +0000

    [Add] deviseルーティング ヘッダー作成

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,8 @@ devise_for :users, skip: [:passwords], controllers: {
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
+root to: "public/homes#top"
+get 'public/homes/about' => "public/homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
