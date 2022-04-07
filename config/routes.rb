98e9cbb021abacd82b8e8eff440379eef9255e2f
umekichi777 <umekichi7teru@gmail.com>
2022-04-07 14:39:01 +0000

    [Add] ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
 # 顧客用
-# URL /users/sign_in ...
 devise_for :users, skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
 
+scope module: :public do
+  root to: "homes#top"
+  get 'about' => "homes#about", as: 'about'
+  resources :users, only: [:index, :show, :edit]
+end
+
 # 管理者用
-# URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
 
-root to: "public/homes#top"
-get 'public/homes/about' => "public/homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
