1fb6876b835051b90c517876a58fe89c1a59ea91
umekichi777 <umekichi7teru@gmail.com>
2022-04-08 17:37:21 +0000

    [Update] public/user/show 修正

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -11,10 +11,16 @@ class Public::UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :kana_name, :user_name, :email, :introduction, :age)
+    params.require(:user).permit(:name, :kana_name, :user_name, :email, :introduction, :age, :profile_image)
   end
 end
