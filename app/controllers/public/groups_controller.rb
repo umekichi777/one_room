4648ce2c4a4b9852fcf26e305e61cfb9abe1eb66
umekichi777 <umekichi7teru@gmail.com>
2022-04-16 17:06:08 +0000

    [Add] group 各view

--- app/controllers/public/groups_controller.rb
+++ app/controllers/public/groups_controller.rb
@@ -1,4 +1,7 @@
 class Public::GroupsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+
   def new
     @group = Group.new
   end
@@ -14,18 +17,37 @@ class Public::GroupsController < ApplicationController
   end
 
   def index
+    @groups = Group.all
   end
 
   def show
+    @group = Group.find(params[:id])
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
-  
-  
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to group_path(@group.id)
+    else
+      render 'edit'
+    end
+  end
+
+
   private
-  
+
   def group_params
     params.require(:group).permit(:name, :introduction, :group_image)
+  end
+
+  def ensure_correct_user
+    @group = Group.find(params[:id])
+    unless @group.owner_id == current_user.id
+      redirect_to groups_path
+    end
   end
 end
