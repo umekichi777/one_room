bcae03e496ad2fce25f253229e86efbcd56aa459
umekichi777 <umekichi7teru@gmail.com>
2022-04-17 14:29:23 +0000

    [Add] tag controller show,edit追加

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,12 +1,16 @@
 class Public::PostsController < ApplicationController
   def new
     @post = Post.new
+    @tag_list = @post.tags.pluck(:name).join(',')
   end
 
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
+    # 受け取った値を , で区切って配列にする
+    tag_list = params[:post][:name].split(',')
     if @post.save
+      @post.save_tag(tag_list)
       redirect_to post_path(@post)
     else
       render 'new'
@@ -22,10 +26,12 @@ class Public::PostsController < ApplicationController
     @post_comment = PostComment.new
     # コメントを最新順に表示
     @post_comments = @post.post_comments.order(created_at: :desc)
+    @post_tags = @post.tags
   end
 
   def edit
     @post = Post.find(params[:id])
+    @tag_list = @post.tags.pluck(:name).join(',')
   end
 
   def update
@@ -36,7 +42,9 @@ class Public::PostsController < ApplicationController
         image.purge
       end
     end
+    tag_list=params[:post][:name].split(',')
     if post.update(post_params)
+      @post.save_tag(tag_list)
       redirect_to post_path(post)
     else
       render 'edit'
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,7 @@
 class Tag < ApplicationRecord
+
+  has_many :posts, through: :post_tags
+  has_many :post_tags, foreign_key: 'tag_id', dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
 end
