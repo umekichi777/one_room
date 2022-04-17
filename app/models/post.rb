a6322efe1866e6ac8125a4ddd46a19af423c8540
umekichi777 <umekichi7teru@gmail.com>
2022-04-17 16:33:51 +0000

    [Add] tag search 一覧追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_many :tags, through: :post_tags
   has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags
 
 
   has_many_attached :images
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,8 @@
 class Tag < ApplicationRecord
 
-  has_many :posts, through: :post_tags
   has_many :post_tags, foreign_key: 'tag_id', dependent: :destroy
+  has_many :posts, through: :post_tags
+
 
   validates :name, presence: true, uniqueness: true
 end
