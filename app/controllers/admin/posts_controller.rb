8a133f47c15f541ec78d38ee34605fc7031e3e86
umekichi777 <umekichi7teru@gmail.com>
2022-04-20 06:20:44 +0000

    [Add] admin_search

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,10 +1,11 @@
 class Admin::PostsController < ApplicationController
-  
+  before_action :set_q, only: [:index, :search]
+
   def index
     @posts = Post.all
     @tag_list = Tag.all
   end
-  
+
   def show
     @post = Post.find(params[:id])
     @post_comment = PostComment.new
@@ -12,7 +13,7 @@ class Admin::PostsController < ApplicationController
     @post_comments = @post.post_comments.order(created_at: :desc)
     @post_tags = @post.tags
   end
-  
+
   def search
     @results = @q.result
   end
@@ -22,13 +23,17 @@ class Admin::PostsController < ApplicationController
     @tag = Tag.find(params[:tag_id])
     @posts = @tag.posts.all
   end
-  
-  
-  
+
+
+
   private
 
   def set_q
     @q = Post.ransack(params[:q])
   end
-  
+
+  def post_params
+    params.require(:post).permit(:title, :body, :user_id, images: [ ])
+  end
+
 end
