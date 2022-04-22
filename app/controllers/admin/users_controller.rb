147326e84c1bb7c84b72532fbf3fd2e8bfe6eb28
umekichi777 <umekichi7teru@gmail.com>
2022-04-22 18:55:32 +0000

    [Add] admin users layouts

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,6 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_q, only: [:index, :search]
+
   def index
     @users = User.all
   end
@@ -7,9 +9,17 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def search
+    @results = @q.result
+  end
+
 
 
   private
+
+  def set_q
+    @q = User.ransack(params[:q])
+  end
 
   def user_params
     params.require(:user).permit(:name, :kana_name, :user_name, :email, :introduction, :age, :profile_image, :is_deleted)
